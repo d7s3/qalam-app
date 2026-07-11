527616291df54bd49e4aa3bc99034bfe412c91d3
faisalMAc <faisalbakhsh07@gmail.com>
2026-07-11 09:08:50 +0300

    feat: implement public and supervisor circle report Livewire components and service layer

---
+++ app/Livewire/Public/CircleReport.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Public;
+
+use Livewire\Component;
+
+class CircleReport extends Component
+{
+    public function render()
+    {
+        return view('livewire.public.circle-report');
+    }
+}
---
+++ app/Livewire/Supervisor/CircleReport.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Supervisor;
+
+use Livewire\Component;
+
+class CircleReport extends Component
+{
+    public function render()
+    {
+        return view('livewire.supervisor.circle-report');
+    }
+}
